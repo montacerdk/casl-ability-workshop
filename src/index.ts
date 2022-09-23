a2e0ac00723fccf69945764e85c31e655d9fd289
Montacer Dkhilali <montacer.dkhilali@unicornpowered.com>
2022-09-23 08:31:57 -0400

    Define abilities between multiple users

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,41 @@ import { AbilitySubject } from "./AbilitySubject";
 const defineAbility = () => {
   const { can, cannot, build } = new AbilityBuilder(Ability);
 
-  can(AbilityAction.Read, AbilitySubject.Posts);
+  const isManager = false;
+  const isGuest = true;
+
+  if (isManager) {
+    can(AbilityAction.Manage, AbilitySubject.Posts);
+    can(AbilityAction.Manage, AbilitySubject.Comments);
+    can(AbilityAction.Manage, AbilitySubject.Users);
+    can(AbilityAction.Manage, AbilitySubject.Videos);
+  }
+
+  if (isGuest) {
+    can(AbilityAction.ReadAll, AbilitySubject.Posts);
+    can(AbilityAction.ReadAll, AbilitySubject.Users);
+    can(AbilityAction.ReadAll, AbilitySubject.Comments);
+    can(AbilityAction.ReadAll, AbilitySubject.Videos);
+  }
 
   return build();
 };
 
 const ability = defineAbility();
 
-const canReadPosts = ability.can(AbilityAction.Read, AbilitySubject.Posts);
-const canWatchVideos = ability.can(AbilityAction.Read, AbilitySubject.Videos);
+const canReadAllPosts = ability.can(
+  AbilityAction.ReadAll,
+  AbilitySubject.Posts
+);
+const canWatchAllVideos = ability.can(
+  AbilityAction.ReadAll,
+  AbilitySubject.Videos
+);
+const canDeleteVideos = ability.can(
+  AbilityAction.Delete,
+  AbilitySubject.Videos
+);
 
-console.log("canReadPosts =======> ", canReadPosts);
-console.log("canWatchVideos =======> ", canWatchVideos);
+console.log("canReadAllPosts =======> ", canReadAllPosts);
+console.log("canDeleteVideos =======> ", canDeleteVideos);
+console.log("canWatchAllVideos =======> ", canWatchAllVideos);
