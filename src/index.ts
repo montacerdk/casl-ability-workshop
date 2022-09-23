ed790fff3b63c4aa8f1e1e87cacc56066c1e5fe9
Montacer Dkhilali <montacer.dkhilali@unicornpowered.com>
2022-09-23 10:44:32 -0400

    Define abilities with conditions

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
-import { Ability, AbilityBuilder } from "@casl/ability";
+import { Ability, AbilityBuilder, subject } from "@casl/ability";
 
 import { AbilityAction } from "./AbilityAction";
 import { AbilitySubject } from "./AbilitySubject";
 
-const defineAbility = () => {
+type User = {
+  id: string;
+  fullName: string;
+  isAdmin?: boolean;
+  isManager?: boolean;
+  isGuest?: boolean;
+};
+
+const defineAbility = (user: User) => {
   const { can, cannot, build } = new AbilityBuilder(Ability);
 
-  const isManager = false;
-  const isGuest = true;
-
-  if (isManager) {
-    can(AbilityAction.Manage, AbilitySubject.Posts);
-    can(AbilityAction.Manage, AbilitySubject.Comments);
-    can(AbilityAction.Manage, AbilitySubject.Users);
-    can(AbilityAction.Manage, AbilitySubject.Videos);
+  if (user.isAdmin) {
+    can(AbilityAction.Manage, AbilitySubject.All);
   }
 
-  if (isGuest) {
-    can(AbilityAction.ReadAll, AbilitySubject.Posts);
+  if (user.isManager) {
+    can(AbilityAction.Manage, AbilitySubject.Posts);
+    can(AbilityAction.Manage, AbilitySubject.Comments);
+    can(AbilityAction.Manage, AbilitySubject.Videos);
+
+    cannot(AbilityAction.Manage, AbilitySubject.Users);
+  }
+
+  if (user.isGuest) {
+    can(AbilityAction.Update, AbilitySubject.Posts, { authorId: user.id });
     can(AbilityAction.ReadAll, AbilitySubject.Users);
     can(AbilityAction.ReadAll, AbilitySubject.Comments);
     can(AbilityAction.ReadAll, AbilitySubject.Videos);
@@ -26,7 +36,23 @@ const defineAbility = () => {
   return build();
 };
 
-const ability = defineAbility();
+class Post {
+  public authorId: string;
+
+  constructor(authorId: string) {
+    this.authorId = authorId;
+  }
+}
+
+const randomPost = new Post("2222");
+
+const user: User = {
+  id: "1111",
+  fullName: "John Doe",
+  isGuest: true,
+};
+
+const ability = defineAbility(user);
 
 const canReadAllPosts = ability.can(
   AbilityAction.ReadAll,
@@ -40,7 +66,12 @@ const canDeleteVideos = ability.can(
   AbilityAction.Delete,
   AbilitySubject.Videos
 );
+const canUpdatePosts = ability.can(
+  AbilityAction.Update,
+  subject(AbilitySubject.Posts, randomPost)
+);
 
 console.log("canReadAllPosts =======> ", canReadAllPosts);
 console.log("canDeleteVideos =======> ", canDeleteVideos);
 console.log("canWatchAllVideos =======> ", canWatchAllVideos);
+console.log("canUpdatePosts =======> ", canUpdatePosts);
